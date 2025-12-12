a7072d620bddd02adbe8a80392773823f6b50d97
Tron <achuan-2@outlook.com>
2025-12-12 23:44:23 +0800

    🎨优化打包脚本

--- build.bat
+++ build.bat
@@ -2,3 +2,4 @@ cd ..
 ZXPSignCmd -selfSignedCert CN Changsha achuan-2.com achuan-2 achuan-2.com adobe证书.p12
 
 ZXPSignCmd  -sign  "D:\Code\illustrator_sci_plugin"  "D:\Code\illustrator_sci_plugin.zxp"  adobe证书.p12 "achuan-2.com" 
+start .
